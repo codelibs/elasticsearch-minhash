91871521eded78c9b96d3515a9823b11b22ad4d8
Shinsuke Sugaya <shinsuke@apache.org>
2019-10-03 06:27:38 +0900

    replace with single-node

--- src/test/java/org/codelibs/elasticsearch/minhash/MinHashPluginTest.java
+++ src/test/java/org/codelibs/elasticsearch/minhash/MinHashPluginTest.java
@@ -39,8 +39,7 @@ public class MinHashPluginTest extends TestCase {
             public void build(final int number, final Builder settingsBuilder) {
                 settingsBuilder.put("http.cors.enabled", true);
                 settingsBuilder.put("http.cors.allow-origin", "*");
-                settingsBuilder.putList("discovery.seed_hosts", "127.0.0.1:9301");
-                settingsBuilder.putList("cluster.initial_master_nodes", "127.0.0.1:9301");
+                settingsBuilder.put("discovery.type", "single-node");
             }
         }).build(newConfigs().clusterName(clusterName).numOfNode(1).pluginTypes("org.codelibs.elasticsearch.minhash.MinHashPlugin"));
 
